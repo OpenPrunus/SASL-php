60e9e50d65de8484d2fb58c9874926a8303c8394
bgounine <bgounine@bah0216.info.local>
2017-09-01 15:57:35 +0200

    [Feature] Add Factory class

---
+++ src/SASL/Exceptions/FactoryException.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace SASL\Exceptions;
+
+/**
+ * Factory exceptions class
+ */
+class FactoryException extends \Exception
+{
+}
---
+++ src/SASL/Factory.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace SASL;
+
+use SASL\Exceptions\FactoryException;
+use SASL\Exceptions\MechanismsException;
+use SASL\Mechanisms\PlainMechanism;
+
+/**
+ * SASL class.
+ * Manage mechanisms types
+ *
+ * @codeCoverageIgnore
+ */
+class SASL
+{
+    /**
+     * @var PlainMechanism
+     */
+    protected $mechanism;
+
+    /**
+     * Constructor
+     *
+     * @param string type
+     *
+     * @return SASL
+     *
+     * @throws FactoryException
+     */
+    public function __construct($type)
+    {
+        if (!is_string($type)) {
+            throw new FactoryException("Argument is not a string");
+        }
+
+        switch (strtolower($type)) {
+            case 'plain':
+                $this->mechanism = new PlainMechanism();
+                break;
+
+            default:
+                throw new FactoryException("Unkown type");
+        }
+    }
+
+    /**
+     * Get formatted string with Mecanism implemented
+     *
+     * @param array $arguments
+     * expected possible keys
+     * (defined in
+     * - RFC4616 https://tools.ietf.org/html/rfc4616
+     * ) :
+     * - authzid
+     * - authcid
+     * - passwd
+     *
+     * @return string
+     *
+     * @throws MechanismsException
+     */
+    public function getFormattedResponse(Array $arguments)
+    {
+        return $this->mechanism->getFormattedResponse($arguments);
+    }
+}
